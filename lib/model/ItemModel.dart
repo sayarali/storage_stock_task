7d1d4483201df5dd6840d29200beb7c2f42c65ec
Ali Sayar <alisayar07@gmail.com>
2023-07-12 10:19:32 +0300

    ItemModel

---
+++ lib/model/ItemModel.dart
@@ -0,0 +1,8 @@
+class ItemModel{
+  String itemBarcode;
+  String itemName;
+  DateTime itemExpiredDate;
+  int itemCount;
+
+  ItemModel(this.itemBarcode, this.itemName, this.itemExpiredDate, this.itemCount);
+}
