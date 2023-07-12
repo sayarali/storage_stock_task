ee5862119e18750914855cf7fa793ad6f44fe02d
Ali Sayar <alisayar07@gmail.com>
2023-07-12 10:19:23 +0300

    StorageModel

---
+++ lib/model/storage_model.dart
@@ -0,0 +1,9 @@
+
+import 'package:storage_stock_task/model/ItemModel.dart';
+
+class StorageModel {
+  String storageName;
+  List<ItemModel> items;
+
+  StorageModel(this.storageName, this.items);
+}
